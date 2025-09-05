12a63da281bd1babcd82ff94833187bc6ad33412
shubbham-kumarr <shubbhammkumarr@outlook.com>
2025-09-05 19:21:02 +0530

    modified hero section

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// theme toggle
 
 const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const topSec = document.getElementById('topSec')
 
-prefersDark ? document.body.classList.add('dark'):document.body.classList.add('light')
+prefersDark ? document.body.classList.add('dark')  :document.body.classList.add('light')  
+prefersDark?topSec.classList.add('dark'):topSec.classList.add('light')
 
 
+console.log(topSec);
+ 
